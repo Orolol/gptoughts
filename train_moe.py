c505f22a1918c8477068ebb2cec6b44e2fd7aa21
Gaetan <gaetan@0r0.fr>
2025-01-07 18:45:54 +0100

    fix : problem with A100

--- train_moe.py
+++ train_moe.py
@@ -63,7 +63,7 @@ data_dir = 'data/openwebtext'
 gradient_accumulation_steps = 1
 dropout = 0.0
 bias = False
-attention_backend = "xformers"
+attention_backend = "sdpa"
 
 # Configure CUDA Graph behavior
 torch._inductor.config.triton.cudagraph_skip_dynamic_graphs = True
