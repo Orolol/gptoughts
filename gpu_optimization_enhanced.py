f021d5405d71b47bc526931ddd7b7570adae1f7e
Gaetan <gaetan@0r0.fr>
2025-03-06 14:58:05 +0100

    feat: fix ampere

--- gpu_optimization_enhanced.py
+++ gpu_optimization_enhanced.py
@@ -84,6 +84,7 @@ def setup_enhanced_cuda_optimizations():
         
         # Optimisations pour les opérations de calcul
         torch.backends.cuda.matmul.allow_fp16_reduced_precision_reduction = True
+        torch.backends.cuda.matmul.allow_bf16_reduced_precision_reduction = True
         
         # Optimisations de mémoire pour GPUs grand public
         os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True,max_split_size_mb:128,garbage_collection_threshold:0.7"
