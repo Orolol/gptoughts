7c450f20327c24b866e55120a878aa5e03321b14
Gaetan <gaetan@0r0.fr>
2025-03-07 15:50:34 +0100

    feat: add apollo

--- run_train.py
+++ run_train.py
@@ -166,6 +166,9 @@ def main():
                        help='Backend d\'attention à utiliser')
     
     # Paramètres d'optimisation
+    parser.add_argument('--optimizer_type', type=str, default=None,
+                       choices=['adamw', 'lion', 'apollo', 'apollo-mini'],
+                       help='Type d\'optimiseur à utiliser (par défaut: adamw pour GPT, lion pour MoE/LLaDA)')
     parser.add_argument('--learning_rate', type=float, default=5e-5,
                        help='Taux d\'apprentissage')
     parser.add_argument('--max_iters', type=int, default=100000,
