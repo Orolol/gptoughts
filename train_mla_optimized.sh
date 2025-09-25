683a95498e64e73b3f4423d5fef9079ca244a214
Gaetan <gaetan@0r0.fr>
2025-09-25 11:05:57 +0200

    feat: cleanup

--- train_mla_optimized.sh
+++ train_mla_optimized.sh
@@ -32,6 +32,7 @@ python run_train.py \
     --block_size $BLOCK_SIZE \
     --output_dir $OUTPUT_DIR \
     --precision bf16-mixed \
+    --optimize_attention \
     --preallocate_memory \
     --grad_clip 1.0 \
     --learning_rate 5e-5 \
